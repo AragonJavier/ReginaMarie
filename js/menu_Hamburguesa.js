5d2310bbebb3c0f12425b28093fd01f8d73aad6c
Alexander <aragonjavier07@gmail.com>
2020-12-26 16:27:18 -0600

    Vbeta

---
+++ js/menu_Hamburguesa.js
@@ -0,0 +1,15 @@
+const d = document;
+
+function hamburgerMenu(panelbtn, panel_aside) {
+  d.addEventListener("click", (e) => {
+    if (e.target.matches(panelbtn)||e.target.matches(`${panelbtn} *`))
+      d.querySelector(panel_aside).classList.toggle("is-active")
+      d.querySelector(panelbtn).classList.toggle("is-active")
+    return
+  })
+}
+d.addEventListener('DOMContentLoaded', e => {
+  hamburgerMenu(".btn_menu", ".header_aside");
+
+})
+console.log(hamburgerMenu)
